6df72f91656223f6bdeb7307af1c865a14c8822f
Naveed Qamar <60569200+naveedqamar650@users.noreply.github.com>
2022-11-24 01:27:47 +0500

    DashBoardModel Updated

--- Smart-ID/Smart-ID/Models/NavigationPagesModels/DashBoardModel.cs
+++ Smart-ID/Smart-ID/Models/NavigationPagesModels/DashBoardModel.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Smart_ID.Models.NavigationPagesModels
 {
-    internal class DashBoardModel
+    public class DashBoardListViewModel
     {
+        public string ImgUrl { get; set; }
+        public string Name { get; set; }
+        public int MemorySpace { get; set; }
+        public int Items { get; set; }
+        public string IconUrl { get; set; }
     }
 }
